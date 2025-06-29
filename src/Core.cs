bcdbf909c9971736ed388f68f980b7dca4294f25
Sina <unpatch@proton.me>
2025-06-29 23:22:17 +0330

    include github link in the assembly

--- src/Core.cs
+++ src/Core.cs
@@ -1,6 +1,6 @@
 ﻿using MelonLoader;
 
-[assembly: MelonInfo(typeof(DeliveryWithCash.Core), "DeliveryWithCash", "1.0.0", "unpatch", null)]
+[assembly: MelonInfo(typeof(DeliveryWithCash.Core), "DeliveryWithCash", "1.0.0", "unpatch", "https://github.com/unpatch/DeliveryWithCash")]
 [assembly: MelonGame("TVGS", "Schedule I")]
 [assembly: MelonColor(0, 157, 255, 82)]
 [assembly: MelonPlatformDomain(MelonPlatformDomainAttribute.CompatibleDomains.IL2CPP)]
